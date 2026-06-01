49c49717963fb619e1ec37e5e46eb6fd5b96f6e5
Bobbi Developer <bobbititharunreddy@gmail.com>
2026-06-01 14:32:52 +0530

    fix: resolve incorrect import path for firebase in Seller_HomePage

--- src/🧾Seller_pages/📁Seller_Homepagethings/Seller_HomePage.jsx
+++ src/🧾Seller_pages/📁Seller_Homepagethings/Seller_HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { CircleUserRound, ShieldAlert, ShieldCheck, ShieldX } from 'lucide-react';
 import Seller_BannerPromo from './Seller_BannerPromo';
-import { db } from '../firebase';
+import { db } from '../../firebase';
 import { doc, getDoc } from 'firebase/firestore';
 
 function Seller_HomePage() {
